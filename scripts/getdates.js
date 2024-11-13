4ca19134e7c6e9032395ffe0eb1d497b6dbf3bfa
christian-mapundu <u298423595@byui.edu>
2024-11-13 11:47:27 +0200

    hardwork.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,3 @@
-alert(document.lastModified);
+const year = document.querySelector("#currentyear");
+const today = new Date();
+currentyear.innerHTML = `last Modification: <span class="highlight">${today}</span>`;
