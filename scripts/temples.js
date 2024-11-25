6de3a9023cdbbc8cebf623d689dff5289d37ba82
christian-mapundu <u298423595@byui.edu>
2024-11-26 00:07:16 +0200

    lll

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,20 @@
-const burgerButton = document.getElementById('burger_menu');
+/*const burgerButton = document.getElementById('burger_menu');
 const navigation = document.querySelector("nav");
 
 burgerButton.addEventListener('click', () => {
     navigation.classlist.toggle('open');
     burgerButton.classlist.toggle('open');
-})
+})*/
+
+
+function toggleMenu() {
+    const navMenu = document.querySelector('nav');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+}
 
 const year = document.querySelector("#currentyear");
 const today = new Date();
