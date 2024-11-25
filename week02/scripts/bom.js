8c74b6ac8c194cec9376f0348ae9940c603dabc2
christian-mapundu <u298423595@byui.edu>
2024-11-25 06:32:39 +0200

    changes to all files have been made.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -8,3 +8,17 @@ li.textContent = input.value;
 deletebutton.textContent = '❌';
 li.append(deletebutton);
 list.append(li);
+
+const addButton = document.getElementById("add-chapter");
+addButton.addEventListener('click', function() {
+    if (input.value.trim() !== ''){
+
+    }
+});
+deletebutton.addEventListener('Click', function(){
+    list.removeChild(li);
+    input.focus();
+})
+input.value = '';
+
+input.focus();
