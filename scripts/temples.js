89d6a7e428eb3b460ab6eba6fa429727977a5584
christian-mapundu <u298423595@byui.edu>
2024-11-25 23:38:33 +0200

    added the @media queries;

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
+const burgerButton = document.getElementById('burger_menu');
+const navigation = document.querySelector("nav");
+
+burgerButton.addEventListener('click', () => {
+    navigation.classlist.toggle('open');
+    burgerButton.classlist.toggle('open');
+})
+
 const year = document.querySelector("#currentyear");
 const today = new Date();
 currentyear.innerHTML = `last Modification: <span class="highlight">${today}</span>`;
