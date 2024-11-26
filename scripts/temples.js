7217478584aba784b908aab0985c2536acfcc58c
christian-mapundu <u298423595@byui.edu>
2024-11-26 02:19:37 +0200

    corrected the ClassList.toggle('open')

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,20 +1,19 @@
-/*const burgerButton = document.getElementById('burger_menu');
+const burgerButton = document.getElementById('burger_menu');
 const navigation = document.querySelector("nav");
 
 burgerButton.addEventListener('click', () => {
-    navigation.classlist.toggle('open');
-    burgerButton.classlist.toggle('open');
-})*/
+    navigation.classList.toggle('open');
+    burgerButton.classList.toggle('open');
+})
 
-
-function toggleMenu() {
-    const navMenu = document.querySelector('nav');
-    if (navMenu.style.display === 'block') {
-        navMenu.style.display = 'none';
-    } else {
-        navMenu.style.display = 'block';
-    }
-}
+// function toggleMenu() {
+//     const navMenu = document.querySelector('nav');
+//     if (navMenu.style.display === 'block') {
+//         navMenu.style.display = 'none';
+//     } else {
+//         navMenu.style.display = 'block';
+//     }
+// }
 
 const year = document.querySelector("#currentyear");
 const today = new Date();
